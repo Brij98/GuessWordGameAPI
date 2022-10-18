ea9c422eebfd9c22c0fc7881a9054b150fb68e22
Charlie Taylor <peekoecode@gmail.com>
2022-10-17 17:02:39 -0700

    PEP8 format plus + hint improvement

--- api/api.py
+++ api/api.py
@@ -30,14 +30,17 @@ class User:
     username: str
     password: str
 
+
 @dataclasses.dataclass
 class UserDTO:
     username: str
     password: str
 
+
 @dataclasses.dataclass
 class Word:
     word: str
+
 
 @dataclasses.dataclass
 class Session:
@@ -47,6 +50,7 @@ class Session:
     movesCompleted: str
     sessionCompleted: bool
 
+
 @dataclasses.dataclass
 class Guess:
     sessionId: int
@@ -54,10 +58,13 @@ class Guess:
     guess: str
     hint: str
 
+
 @dataclasses.dataclass
 class GuessDTO
-    sessionId: int
-    guess: str
+
+
+sessionId: int
+guess: str
 
 
 # Database connections on demand
@@ -72,6 +79,7 @@ async def _get_db():
         await db.connect()
     return db
 
+
 @app.route("/user", methods=["POST"])
 @validate_request(UserDTO)
 async def create_user(data):
@@ -84,48 +92,36 @@ async def create_user(data):
         id = await db.execute("INSERT INTO Users VALUES(:username, :password)", user)
     except sqlite3.IntegrityError as e:
         abort(409, e)
-    
+
     user["id"] = id
     return book, 201, {"msg": "Successfully created account"}
 
+
 @app.route("/user/(str:username)/(str:password)", methods=["GET"])
-async def check_password(username, password):
+async def check_password(username: str, password: str):
     db = await _get_db()
 
     try:
         user = await db.fetch_one(
-            "SELECT * FROM User WHERE username = :username", 
+            "SELECT * FROM User WHERE username = :username",
             values={"username": username})
     except:
         abort(404)
-    
-    return 200, { "authenticated", hash(password) == user.password }
 
-def auth_required(func):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        auth = request.authorization
-        db = _get_db()
-        user = await db.fetch_one(
-            "SELECT * FROM User WHERE username = :username", 
-            values={"username": auth.username})
-        
-        if (
-            auth is not None and 
-            auth.type == "basic" and
-            auth.username == user.username and
-            hash(auth.password) == user.password
-        ):
-            return await func(*args, **kwargs)
-        else:
-            abort(401)
+    return 200, {"authenticated", hash(password) == user.password}
 
-    return wrapper
 
 @auth_required
 @app.route("/session", methods=["POST"])
 async def create_session(data):
     pass
+
+
+@auth_required
+@app.route("/guess/(int:sessionId)/(str:guess)")
+async def guess(sessionId: int, guess: str):
+    pass
+
 
 @app.route("/books/<int:id>", methods=["GET"])
 async def one_book(id):
@@ -136,12 +132,43 @@ async def one_book(id):
     else:
         abort(404)
 
+
+def auth_required(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        auth = request.authorization
+        db = _get_db()
+        user = await db.fetch_one(
+            "SELECT * FROM User WHERE username = :username",
+            values={"username": auth.username})
+
+        if (
+            auth is not None and
+            auth.type == "basic" and
+            auth.username == user.username and
+            hash(auth.password) == user.password
+        ):
+            return await func(*args, **kwargs)
+        else:
+            abort(401)
+
+    return wrapper
+
+
 def create_hint(guess: str, word: str) -> str:
+    if guess == word:
+        return 'Guess is correct!'
+
+    if len(guess) != 5:
+        return 'Invalid guess!'
+
     guess, word = guess.upper(), word.upper()
     guessSet, wordSet = set(guess), set(word)
     difference = guessSet.difference(wordSet)
     states = []
 
+    # 2 is same pos, 1 is wrong pos, 0 is not in word
+    # create array that has state for each char
     for i in range(5):
         if guess[i] == word[i]:
             states.append(2)
@@ -150,7 +177,7 @@ def create_hint(guess: str, word: str) -> str:
         if guess[i] in wordSet:
             states.append(1)
             continue
-        
+
         states.append(0)
 
     hint = []
@@ -164,12 +191,13 @@ def create_hint(guess: str, word: str) -> str:
             hint.append(f"{guess[i]} not in {i + 1},")
             guessSet.remove(guess[i])
 
+    # add hints for chars not in word
     for char in difference:
         hint.append(char)
-    
+
     if len(difference):
         hint.append("not in word")
     else:
         hint[-1] = hint[-1][:-1]
 
-    return  " ".join(hint)
+    return " ".join(hint)
